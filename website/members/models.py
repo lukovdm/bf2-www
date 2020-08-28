0e1890484172e382d7f6330535eb3ac27f1adde2
Luko van der Maas <me@luko.dev>
2020-08-28 15:01:10 +0200

    make initial member model

---
+++ website/members/models.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Member(models.Model):
+    """A model to hold all extra personal information about members."""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    birthday = models.DateField()
+
+    phone_number = models.CharField(max_length=16)
+
+    street_address = models.CharField(max_length=255, verbose_name=_("Street address and house number"))
+
+    postcode = models.CharField(max_length=7, validators=[RegexValidator("^\\d{4} ?[A-Za-z]{2}$")])
+
+    city = models.CharField(max_length=52)
+
+    is_student = models.BooleanField()
+
+    sports_card_number = models.CharField(max_length=10)
+
+    graduation_date = models.DateField()
--- website/website/settings.py
+++ website/website/settings.py
@@ -96,6 +96,7 @@ INSTALLED_APPS = [
     'djangocms_bootstrap4.contrib.bootstrap4_picture',
     'djangocms_bootstrap4.contrib.bootstrap4_tabs',
     'djangocms_bootstrap4.contrib.bootstrap4_utilities',
+    'members',
 ]
 
 MIDDLEWARE = [

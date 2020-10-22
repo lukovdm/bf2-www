2e1f7b868258ac0548b8f611276d5b5b659de96c
Luko van der Maas <me@luko.dev>
2020-10-22 16:28:50 +0200

    run black

--- website/website/settings/production.py
+++ website/website/settings/production.py
@@ -3,7 +3,7 @@ import os
 from website.settings.base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -13,18 +13,18 @@ ALLOWED_HOSTS = ["frisbeenijmegen.nl", "bf2.luko.dev", "localhost"]
 SESSION_COOKIE_SECURE = True
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': int(os.environ.get('DATABASE_PORT', 5432)),
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "PORT": int(os.environ.get("DATABASE_PORT", 5432)),
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
     }
 }
 
-STATIC_ROOT = '/bf2-www/static/'
-STATIC_URL = '/static/'
+STATIC_ROOT = "/bf2-www/static/"
+STATIC_URL = "/static/"
 
-MEDIA_ROOT = '/bf2-www/media'
-MEDIA_URL = '/media/'
+MEDIA_ROOT = "/bf2-www/media"
+MEDIA_URL = "/media/"

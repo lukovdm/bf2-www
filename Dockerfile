3f823eb8029a11d36eefe32d4d0551c93f75b912
luko van der maas <lukovdm@users.noreply.github.com>
2020-10-22 20:59:29 +0200

    Add a docker compose configuration for production (#6)
    * Getting started on a production system, with docker
    
    * Working docker-compose setup
    
    * run black
    
    * change nginx port
    
    * Add certbot to dockercompose and add ssl to nginx
    
    * Add proper version of cerbot and ssl
    
    * working version of nginx config

---
+++ Dockerfile
@@ -0,0 +1,44 @@
+# Use the official Python image from the Docker Hub
+FROM python:3.8
+
+# These two environment variables prevent __pycache__/ files.
+ENV PYTHONUNBUFFERED=1 \
+    PYTHONDONTWRITEBYTECODE=1 \
+    PIP_NO_CACHE_DIR=off \
+    PIP_DISABLE_PIP_VERSION_CHECK=on \
+    PIP_DEFAULT_TIMEOUT=100 \
+    POETRY_VERSION=1.1.3 \
+    POETRY_VIRTUALENVS_IN_PROJECT=true \
+    POETRY_NO_INTERACTION=1
+# Use production settings
+ENV DJANGO_SETTINGS_MODULE website.settings.production
+# Add poetry to PATH
+ENV PATH="/root/.poetry/bin:$PATH"
+
+RUN apt-get update \
+    && apt-get install --no-install-recommends -y \
+        # deps for installing poetry
+        curl \
+        # deps for building python deps
+        build-essential \
+        # deps for postgress interaction
+        postgresql-client
+
+# install poetry - respects $POETRY_VERSION & $POETRY_HOME
+RUN curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
+
+WORKDIR /bf2-www
+
+# Copy requirements
+COPY pyproject.toml poetry.lock ./
+
+# Install requirements for production
+RUN poetry install --no-ansi --no-dev --extras "production"
+
+# Copy entrypoint
+COPY config/entrypoint.sh ./
+
+# Copy the rest of the code. 
+COPY website ./
+
+ENTRYPOINT ["./entrypoint.sh"]
--- website/website/settings/base.py
+++ website/website/settings/base.py
@@ -17,18 +17,6 @@ from django.contrib.messages import constants as message_constants
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "x%ts1gsfjnb&2-s4567^*$82ukdr-!%c)3t3yf&&wa3o9636uq"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 SITE_ID = 1
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
@@ -153,17 +141,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "website.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -200,12 +177,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = "/static/"
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")

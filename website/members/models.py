14efbd8643957e693f8c1837953d18ea9d4f3d86
NienkePienke01 <76436564+NienkePienke01@users.noreply.github.com>
2020-12-21 17:23:25 +0100

    added a run or han student box (#17)
    * added a run or han student box
    
    * Added a han/ru thing in the become member
    
    * Added required field

--- website/members/models.py
+++ website/members/models.py
@@ -2,10 +2,18 @@ from django.conf import settings
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
 
 class Member(models.Model):
     """A model to hold all extra personal information about members."""
+
+    RU = "RU"
+    HAN = "HAN"
+    TYPE_STUDENT = [
+        (RU, _("radboud university")),
+        (HAN, _("HAN")),
+    ]
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
@@ -25,6 +33,16 @@ class Member(models.Model):
 
     is_student = models.BooleanField()
 
+    student_type = models.CharField(
+        max_length=3, choices=TYPE_STUDENT, blank=True, null=True
+    )
+
     sports_card_number = models.CharField(max_length=10)
 
     graduation_date = models.DateField()
+
+    def clean(self):
+        if self.is_student == True and self.student_type is None:
+            raise ValidationError(
+                _("You are a student so please indicate your student type")
+            )

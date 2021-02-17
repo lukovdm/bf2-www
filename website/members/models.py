2eae876c3e4d57e94c4afd0c8cf5cb003c2fe257
luko van der maas <lukovdm@users.noreply.github.com>
2021-02-17 21:10:34 +0100

    Add ability to translate website (#35)
    * Add translations for hardcoded text
    
    * added migrations for multilingual fields and translations to the board
    
    * fix capfirst mistake in template
    
    * run black
    
    * fix a few translations and add translations ci
    
    * add translation ci this time for real
    
    * fix fuzzy translations
    
    * add extra output to ci
    
    * fix fuzzy in members
    
    * more ci output

--- website/members/models.py
+++ website/members/models.py
@@ -17,32 +17,40 @@ class Member(models.Model):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    birthday = models.DateField()
+    birthday = models.DateField(verbose_name=_("birthday"))
 
-    phone_number = models.CharField(max_length=16)
+    phone_number = models.CharField(max_length=16, verbose_name=_("phone number"))
 
     street_address = models.CharField(
-        max_length=255, verbose_name=_("Street address and house number")
+        max_length=255, verbose_name=_("street name and house number")
     )
 
     postcode = models.CharField(
-        max_length=7, validators=[RegexValidator("^\\d{4} ?[A-Za-z]{2}$")]
+        max_length=7,
+        validators=[RegexValidator("^\\d{4} ?[A-Za-z]{2}$")],
+        verbose_name=_("postcode"),
     )
 
-    city = models.CharField(max_length=52)
+    city = models.CharField(max_length=52, verbose_name=_("city"))
 
-    is_student = models.BooleanField()
+    is_student = models.BooleanField(verbose_name=_("is student"))
 
     student_type = models.CharField(
-        max_length=3, choices=TYPE_STUDENT, blank=True, null=True
+        max_length=3,
+        choices=TYPE_STUDENT,
+        blank=True,
+        null=True,
+        verbose_name=_("student type"),
     )
 
-    sports_card_number = models.CharField(max_length=10)
+    sports_card_number = models.CharField(
+        max_length=10, verbose_name=_("sports card number")
+    )
 
-    graduation_date = models.DateField()
+    graduation_date = models.DateField(verbose_name=_("graduation date"))
 
     def clean(self):
-        if self.is_student == True and self.student_type is None:
+        if self.is_student and self.student_type is None:
             raise ValidationError(
                 _("You are a student so please indicate your student type")
             )

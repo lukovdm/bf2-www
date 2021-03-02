79b351ed81b57c9ad0f3a41d05d6754e176b4f9e
NienkePienke01 <nienke.muskee@home.nl>
2021-03-02 20:45:00 +0100

    black thingie

--- website/website/settings/development.py
+++ website/website/settings/development.py
@@ -24,9 +24,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")

e974a9545b81b587b68fffc8152c04411c20ae32
Luko van der Maas <me@luko.dev>
2021-03-02 23:16:10 +0100

    Clean up user admin and add preliminary import-export

--- website/members/models.py
+++ website/members/models.py
@@ -15,7 +15,7 @@ class Member(models.Model):
         (HAN, _("HAN")),
     ]
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     birthday = models.DateField(verbose_name=_("birthday"))
 

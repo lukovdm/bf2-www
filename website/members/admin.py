3a048db3758485b34c6c815d9daad1caf6166ffc
NienkePienke01 <76436564+NienkePienke01@users.noreply.github.com>
2021-03-02 21:19:55 +0100

    Become member form (#45)
    * added the other club option
    
    * still added other club option
    
    * did the black thing
    
    * added unique property
    
    * did the migrations

--- website/members/admin.py
+++ website/members/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Member
+from .models import Member, OtherClub
 
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OtherClub)
+class OtherClubAdmin(admin.ModelAdmin):
     pass

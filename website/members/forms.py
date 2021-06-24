9edcf3c88c1ce348d68d17b281a6ab3bea3ceff1
NienkePienke01 <76436564+NienkePienke01@users.noreply.github.com>
2021-06-24 17:55:06 +0200

    Mail to seccie by sign up (#56)
    * added sending email to seccie
    
    * black
    
    * changed the remarks
    
    * some enters
    
    * po file
    
    * added the missing vars
    
    * black
    
    * description
    
    * merge conflicts

--- website/members/forms.py
+++ website/members/forms.py
@@ -6,6 +6,11 @@ from django.forms import (
     PasswordInput,
     BooleanField,
 )
+from django_mail_template.models import Configuration
+from django.core.exceptions import ValidationError
+from django.core.mail import mail_admins
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 
 from members.models import Member
 
@@ -36,4 +41,41 @@ class BecomeAMemberForm(ModelForm):
         if commit:
             member.save()
             self.save_m2m()
+
+            template = Configuration.get_mail_template("new_member")
+            if template:
+                link = reverse("admin:auth_user_change", args=(user.id,))
+                template.send(
+                    {
+                        "name": user.get_full_name(),
+                        "phone_number": member.phone_number(),
+                        "email": user.email(),
+                        "google_email": member.google_email(),
+                        "birthday": member.birthday(),
+                        "sports_card_number": member.sports_card_number(),
+                        "link_to_member": link,
+                        "pronouns": member.pronouns(),
+                    }
+                )
+            else:
+                mail_admins(
+                    """No template or configuration for sign up""",
+                    """Someone signed up using the sign up form on the website. But either the configuration is missing or there is no 
+                    template connected to this configuration. Please solve this problem now!""",
+                )
+                if not Configuration.objects.filter(process="new_member").exists():
+                    Configuration.objects.create(
+                        process="new_member",
+                        description="""This configuration is used to send a mail to the secretary when a new member signed up.
+                        You can use the following variables: \n
+                        {name}: the name of the new member \n
+                        {phone_number}: the phone number of the member \n
+                        {email}: the email of the member \n
+                        {google_email}: the google email of the member \n
+                        {birthday}: the birthday of the member \n
+                        {sports_card_number}: the sports card number of the member \n 
+                        {link_to_member}: the link the this specific member \n
+                        {pronouns}: the pronouns of the member \n
+                        """,
+                    )
         return member

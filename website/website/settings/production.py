cd0611632bb76558f6ffd701e8e279000cb9644a
luko van der maas <lukovdm@users.noreply.github.com>
2021-08-14 17:31:24 +0200

    add new domains to relevant files

--- website/website/settings/production.py
+++ website/website/settings/production.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["frisbeenijmegen.nl", "bf2.luko.dev", "localhost"]
+ALLOWED_HOSTS = [".bfrisbee2s.nl", "bf2.luko.dev", "localhost"]
 
 SESSION_COOKIE_SECURE = True
 

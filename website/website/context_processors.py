de64da94bf8877be95bfc03fef5bed6aeb16eb20
luko van der maas <lukovdm@users.noreply.github.com>
2021-09-01 13:16:21 +0200

    Hotfix/boards (#123)
    * fixed migration
    
    * add default to migration
    
    * fix randrange bug

--- website/website/context_processors.py
+++ website/website/context_processors.py
@@ -5,4 +5,8 @@ from website.models import Slideshow
 
 def add_slideshow_context(request):
     slides = Slideshow.objects.all()
-    return {"slideshow": slides, "active_index": random.randint(0, len(slides) - 1)}
+    if len(slides) == 0:
+        active_index = 0
+    else:
+        active_index = random.randint(0, len(slides) - 1)
+    return {"slideshow": slides, "active_index": active_index}

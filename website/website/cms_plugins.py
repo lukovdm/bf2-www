bd6d2202c37cd68dffd283dcb5e956bfafd3b99b
NienkePienke01 <nienke.muskee@home.nl>
2021-11-30 15:36:17 +0100

    OnlyLoggedIn

---
+++ website/website/cms_plugins.py
@@ -0,0 +1,12 @@
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from cms.models.pluginmodel import CMSPlugin
+from django.utils.translation import gettext_lazy as _
+
+@plugin_pool.register_plugin
+class ParentCMSPlugin(CMSPluginBase):
+    render_template = 'ShowLoggedIn.html'
+    name = 'ShowLoggedIn'
+    allow_children = True 
+    cache = False 
+    

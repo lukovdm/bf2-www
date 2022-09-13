854c6dfc8c02c601f6c40f5bffa1e64dc0ea4442
Luko van der Maas <me@luko.dev>
2022-09-13 19:04:39 +0200

    fix csrf issue

--- website/website/settings/production.py
+++ website/website/settings/production.py
@@ -6,7 +6,7 @@ from website.settings.base import *
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".bfrisbee2s.nl", "bf2.luko.dev", "localhost"]
 

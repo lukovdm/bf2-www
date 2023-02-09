92bc27bb595964d8d0db4d198dcd4eee71fe2274
luko van der maas <lukovdm@users.noreply.github.com>
2023-02-09 11:57:55 +0100

    Add ics link to events (#204)

--- website/events/urls.py
+++ website/events/urls.py
@@ -6,10 +6,12 @@ from events.views import (
     EventRegisterView,
     EventUnregisterView,
     EventFormView,
+    EventICSView,
 )
 
 urlpatterns = [
     path("", EventListView.as_view(), name="list"),
+    path("events.ics", EventICSView.as_view(), name="ics"),
     path("<int:pk>/", EventDetailView.as_view(), name="detail"),
     path("<int:pk>/register/", EventRegisterView.as_view(), name="register"),
     path("<int:pk>/form", EventFormView.as_view(), name="form"),

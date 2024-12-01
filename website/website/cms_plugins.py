003eb7c010aa5b816432c4184e315c51226e5bef
Luko van der Maas <me@luko.dev>
2024-12-01 16:22:25 +0100

    initial version without forms working

--- website/website/cms_plugins.py
+++ website/website/cms_plugins.py
@@ -1,8 +1,5 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-from cms.models.pluginmodel import CMSPlugin
-from django.utils.translation import gettext_lazy as _
-from djangocms_file.cms_plugins import FilePlugin
 
 
 @plugin_pool.register_plugin
@@ -11,12 +8,3 @@ class OnlyLoggedInPlugin(CMSPluginBase):
     name = "ShowLoggedIn"
     allow_children = True
     cache = False
-
-
-@plugin_pool.register_plugin
-class BetterFilePlugin(FilePlugin):
-    name = _("Better File")
-    allow_children = True
-
-    def get_render_template(self, context, instance, placeholder):
-        return "djangocms_file_better/{}/file.html".format(instance.template)

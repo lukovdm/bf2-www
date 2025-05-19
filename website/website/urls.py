8640dc494bc4aa598ecd27aaf0f5163d819421b5
Jaap Aarts <jaap.aarts1@gmail.com>
2025-05-19 20:35:47 +0200

    black

--- website/website/urls.py
+++ website/website/urls.py
@@ -25,8 +25,10 @@ from django.views.defaults import page_not_found, server_error
 from members.sitemaps import BecomeAMemberSitemap
 from members.views import BecomeAMemberView, PasswordSetView
 
+
 def custom_page_not_found(request):
     return page_not_found(request, None)
+
 
 def custom_server_error(request):
     return server_error(request)
